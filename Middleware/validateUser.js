eaf81fe1165e76a6788693e89ab7845e158eff6a
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2024-11-26 20:08:09 +0530

    middleware for getting user details from token

---
+++ Middleware/validateUser.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+
+const secretKey = process.env.SECRET - KEY || "secret-key";
+
+const validateUser = (req, res, next) => {
+  try {
+    // get the token from the req.header to verify
+    const token = req.header.token;
+
+    // check if there is a token
+    if(!token) {
+        return res.json({verified : false, message : 'No token provided'});
+    }
+
+    // verify the token
+    const decoded = jwt.verify(token, secretKey);
+
+    // get the role and id of the user, role means whether it is Admin, Moderator or User
+    const role = decoded.role;
+    const id = decoded.id;
+
+    // set the role and id in the req.user object
+    req.user = {role, id};
+
+    // call the next middleware to move forward
+    next();
+  } catch (error) {
+    console.log('Error validating user:', error);
+  }
+};
